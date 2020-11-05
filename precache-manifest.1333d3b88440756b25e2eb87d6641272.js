2655b8e867b07e40374b12e7d97cea1e84a7a644
sugihara10969 <sugihara.10969@gmail.com>
2020-11-05 18:08:47 +0900

    Updates

---
+++ precache-manifest.1333d3b88440756b25e2eb87d6641272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61a87bd958302b33037a1416809398a",
+    "url": "/cook-update-sample/index.html"
+  },
+  {
+    "revision": "7014dc480709b1dd692c",
+    "url": "/cook-update-sample/static/css/2.67246356.chunk.css"
+  },
+  {
+    "revision": "7014dc480709b1dd692c",
+    "url": "/cook-update-sample/static/js/2.b6325854.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/cook-update-sample/static/js/2.b6325854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758693e81a67385a7b20",
+    "url": "/cook-update-sample/static/js/main.60fc236b.chunk.js"
+  },
+  {
+    "revision": "decb0fd3be1079f36cde",
+    "url": "/cook-update-sample/static/js/runtime-main.10c5872b.js"
+  }
+]);
